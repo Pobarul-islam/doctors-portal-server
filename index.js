e502342ab30addb1d4fb14cd5a8aea081c6bd842
unknown <Pobarul-islam>
2022-05-23 14:14:51 +0600

    doctors portal server added

---
+++ index.js
@@ -0,0 +1,28 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER}@cluster0.ysxta.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Doctors Portal App Rnning ${port}`)
+})
