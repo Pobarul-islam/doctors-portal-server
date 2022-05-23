213f186fd9c3ce64a02cced3c7866d005c85bb98
unknown <Pobarul-islam>
2022-05-23 14:58:03 +0600

    localhost change

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
         console.log('Database connected')
 
 
