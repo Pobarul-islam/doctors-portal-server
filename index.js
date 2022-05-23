c6b0a563fee538ab3a82c12aa22e4e7b87cd5425
unknown <Pobarul-islam>
2022-05-23 14:44:04 +0600

    database connected

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,20 +10,31 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER}@cluster0.ysxta.mongodb.net/?retryWrites=true&w=majority`;
+// 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ysxta.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run() {
 
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database connected')
+
+
+    }
+    finally {
+
+    }
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello From Doctor Uncle!')
 })
 
 app.listen(port, () => {
-    console.log(`Doctors Portal App Rnning ${port}`)
+    console.log(`Doctors App listening on port ${port}`)
 })
