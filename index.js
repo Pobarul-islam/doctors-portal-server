ebd34eb74ef21e96443b07de2fc88b7a4c773d30
unknown <Pobarul-islam>
2022-05-24 11:25:51 +0600

    bookeng added to backend

--- index.js
+++ index.js
@@ -20,13 +20,42 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('booking');
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
-        console.log('Database connected')
+
+
+        /**
+ * API Naming Convention
+ * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+ * app.get('/booking/:id') // get a specific booking 
+ * app.post('/booking') // add a new booking
+ * app.patch('/booking/:id) //
+ * app.delete('/booking/:id) //
+*/
+
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient;
+            const query = { patient: patient };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+        })
 
 
     }
